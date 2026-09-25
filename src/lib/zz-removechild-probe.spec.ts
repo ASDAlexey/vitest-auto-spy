873d6ab3ff474448994186683e3a6a10f8062dac
Alexey Popov <asdalexey@yandex.ru>
2026-09-25 09:50:14 +0300

    test: remove probe for removeChild

--- src/lib/zz-removechild-probe.spec.ts
+++
@@ -1,9 +0,0 @@
-import { createSpyFromInstance } from './create-spy-from-instance';
-import { setSpyStrict } from './strict';
-
-it('probe', () => {
-  const el = document.createElement('div');
-  document.body.append(el);
-  createSpyFromInstance(el, ['addEventListener']).addEventListener.mockImplementation(() => undefined);
-  expect(() => document.body.removeChild(el)).not.toThrow();
-});
